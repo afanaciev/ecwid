c92c61d1e77e222d153d4eb0492252fe56d4134c
dignityinside <dignityinside@gmail.com>
2012-12-12 17:43:03 +0100

    Версия 1.3

---
+++ shop.php
@@ -0,0 +1,16 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+
+/**
+ *
+ * Alexander Schilling
+ * (c) http://alexanderschilling.net
+ *
+ */
+
+require(getinfo('template_dir') . 'main-start.php');
+
+ecwid();
+
+require(getinfo('template_dir') . 'main-end.php');
+
+#end of file
